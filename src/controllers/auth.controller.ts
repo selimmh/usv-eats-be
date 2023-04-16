7e903cb080bc36e5ac4b4b405fc496ca36f0ee3e
selimmh <selim.mht@gmail.com>
2023-04-16 21:01:28 +0300

    working signup with token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { createUser, getUserByEmail } from "../services/auth.service";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const signUp = async (req: Request, res: Response) => {
   try {
@@ -10,7 +11,9 @@ export const signUp = async (req: Request, res: Response) => {
       return res.status(400).json({ message: "Email already in use" });
     }
     const user = await createUser(name, email, password);
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string);
+    const token = jwt.sign({ id: user._id }, JWT_SECRET as string, {
+      expiresIn: "1h",
+    });
     res.json({ user, token });
   } catch (error) {
     console.error(error);
@@ -29,7 +32,7 @@ export const signIn = async (req: Request, res: Response) => {
     if (!match) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string);
+    const token = jwt.sign({ id: user._id }, JWT_SECRET as string);
     res.json({ user, token });
   } catch (error) {
     console.error(error);
